79c8f78dd60dbd3af7f58e3ebf161e27e1abf4c1
Brandon <brandon@tendency.me>
2024-03-27 06:41:39 -0500

    🔧 Change `auth` middleware to `api` 🔧 Add `web` middleware example

--- app/Bot.php
+++ app/Bot.php
@@ -12,8 +12,12 @@ class Bot extends Laracord
      */
     public function routes(): void
     {
-        Route::middleware('auth')->group(function () {
-            // Route::get('/', fn () => collect($this->registeredCommands)->map(fn ($command) => [
+        Route::middleware('web')->group(function () {
+            // Route::get('/', fn () => 'Hello world!');
+        });
+
+        Route::middleware('api')->group(function () {
+            // Route::get('/commands', fn () => collect($this->registeredCommands)->map(fn ($command) => [
             //     'signature' => $command->getSignature(),
             //     'description' => $command->getDescription(),
             // ]));
