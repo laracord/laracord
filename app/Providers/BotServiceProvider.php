6dbee5043e97d4ed1ba36621304118ae55b4ad12
Brandon <brandon@tendency.me>
2025-02-06 08:13:46 -0600

    🎨 Update the `BotServiceProvider` to reflect framework changes

--- app/Providers/BotServiceProvider.php
+++ app/Providers/BotServiceProvider.php
@@ -2,27 +2,31 @@
 
 namespace App\Providers;
 
+use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Routing\Router;
+use Laracord\Commands\HelpCommand;
+use Laracord\Laracord;
 use Laracord\LaracordServiceProvider;
 
 class BotServiceProvider extends LaracordServiceProvider
 {
     /**
-     * Bootstrap any application services.
-     *
-     * @return void
+     * Configure the bot instance.
      */
-    public function boot()
+    public function bot(Laracord $bot): Laracord
     {
-        parent::boot();
-    }
-
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        parent::register();
+        return $bot
+            ->plugins([
+                //
+            ])
+            ->registerCommands([
+                HelpCommand::class,
+            ])
+            ->withRoutes(function (Router $router) {
+                //
+            })
+            ->withMiddleware(function (Middleware $middleware) {
+                //
+            });
     }
 }
